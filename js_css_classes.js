39a4cf7e39fdbafb06dcd76b7a405ec99fb5821b
Arjan de Ruijter <rra@mboutrecht.nl>
2017-01-13 11:28:37 +0100

    classes

--- js_css_classes.js
+++ js_css_classes.js
@@ -1,5 +1,36 @@
 $(document).ready(function(){
-   $("button").click(function(){
+   $("button").eq(0).click(function(){
       $("tr:nth-child(odd)").addClass("oddRows");
    });
+   
+   $("button").eq(1).click(function(){
+      $("tr:nth-child(odd)").removeClass("oddRows");
+   });
+
+   $("button").eq(2).click(function(){
+      $("tr:nth-child(odd)").toggleClass("oddRows");
+   });
+
+   var choose = "off";
+   var id;
+   $("button").eq(3).click(function(){
+      if (choose == "off")
+      {
+         id = window.setInterval(autoToggle, 1000);
+         choose = "on";
+      }
+      else if (choose == "on" )
+      {
+         clearInterval(id);
+         choose = "off";
+      }
+   });
+   
+   
+
+   function autoToggle(){
+      $("button").eq(2).click();
+   }
+
+
 });
