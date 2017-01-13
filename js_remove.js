51e2cfb1f367b2f9077c77751ef13a692a8a0601
Arjan de Ruijter <rra@mboutrecht.nl>
2017-01-13 09:03:07 +0100

    remove en empty

---
+++ js_remove.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+   /*
+   var popArtistsDied = ["David Bowie", "Prince", "George Michael", "Leonard Cohen",
+                         "Toots Thielemans", "Muhammad Ali", "John Berry", "George Martin",
+                         "Eddy Wally", "Vanity 6", "Robert Stigwood", "Glen Frey"];
+   */
+   var popArtistsDied = [{"name": "David Bowie", "photoName": "David Bowie.jpg"},
+                         {"name": "Prince", "photoName": "David Bowie.jpg"},
+                         {"name": "George Michael", "photoName": "David Bowie.jpg"},
+                         {"name": "Leonard Cohen", "photoName": "David Bowie.jpg"},
+                         {"name": "Toots Thielemans", "photoName": "David Bowie.jpg"},
+                         {"name": "Muhammad Ali", "photoName": "David Bowie.jpg"},
+                         {"name": "John Berry", "photoName": "David Bowie.jpg"},
+                         {"name": "George Martin", "photoName": "David Bowie.jpg"},
+                         {"name": "Eddy Wally", "photoName": "David Bowie.jpg"},
+                         {"name": "Vanity 6", "photoName": "David Bowie.jpg"},
+                         {"name": "Robert Stigwood", "photoName": "David Bowie.jpg"},
+                         {"name": "Glen Frey", "photoName": "David Bowie.jpg"}];
+
+   $("button").click(function(){
+
+      $("ol").empty();
+
+      for ( var i = 0; i < 20; i++)
+      {
+         var index = Math.random() * popArtistsDied.length;
+         index = Math.floor(index);
+
+         $("ol").append("<li><span>" + popArtistsDied[index].name + "</span></li>");
+         $("li:last span").click(function(){
+            $(this).parent().remove();
+         }).mouseenter(function(){
+            $(this).css({"cursor": "pointer", "background-color": "grey"});
+            // Selector maken voor je img tag
+            // attr src
+            // 
+            var test = popArtistsDied.find(function(){
+               return popArtistsDied.name === $(this).text();
+            });
+            $("img").attr({"src": "./images/" + ,
+                           "alt": $(this).text()});
+
+
+         }).mouseleave(function(){
+            $(this).css({"cursor": "pointer", "background-color": "white"});
+         });
+         
+      }      
+   });
+
+   
+   
+   
+
+   //alert("Gelukkig 2017!" + popArtistsDied.length);
+});
+
